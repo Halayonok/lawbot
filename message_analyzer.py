0990a274793a3513a7703a9cdbbd722f23d0f529
Aleksey Halayonok <aleksey.halayonok@draytus.com>
2020-03-08 22:58:36 +0300

    Refactor

--- message_analyzer.py
+++ message_analyzer.py
@@ -14,6 +14,18 @@ codes = {
     "upk" : ['УПК', 'упк', 'Упк', 'уголовный процессуальный кодекс'],
 }
 
+empty_results = {
+    "uk" : "Уголовном кодексе",
+    "gk" : "Гражданском кодексе",
+    "gr_pk" : "Гражданском процессуальном кодексе",
+    "hoz_pk" : "Хозяйственном процессуальном кодексе",
+    "koap" : "Кодексе об административных правонарушениях",
+    "kobs" : "Кодесе о браке и семье",
+    "pikoap" : "Процессуально-исполнительном кодексе об административных правонарушениях",
+    "tk" : "Трудовом кодексе",
+    "upk" : "Уголовно-процессуальном кодексе",
+}
+
 def get_article(splitted_message):
     for item in splitted_message:
         if bool(re.search(r'\d', item)):
@@ -25,18 +37,20 @@ def is_search_for_article(message):
 
 def splitCode(codeName):
     with open(f"/app/{codeName}.txt", 'r', encoding="utf-8") as file:
-        prefixes = ("ВА", "ДЕЛ", "НАЯ", "ТЬ")
+        prefixes = ("ВА", "ДЕЛ", "НАЯ", "ТЬ", "идент", "жение")
         data = file.read()
-        articles = re.split('Статья |ГЛА|РАЗ|ОСОБЕН|ЧАС', data)
+        articles = re.split('Статья |ГЛА|РАЗ|ОСОБЕН|ЧАС|През|Прило', data)
         [articles.remove(s) for s in articles[:] if s.startswith(prefixes) or not s[0].isdigit()]
         return articles
 
 def findArticle(number, code):
-    article = next(a for a in code if a.startswith(number))
+    article = next((a for a in code if a.startswith(number)), "")
+    if len(article) == 0:
+        return article
     return f"Статья {article}"
 
 def findWord(word, code):
-    articles = list(filter(lambda x: word in x, code))
+    articles = list(filter(lambda x: word in x and word not in x.split(".")[0], code))
     full_articles = []
     for item in articles:
         full_articles.append("Статья " + item + "\n")
@@ -51,7 +65,10 @@ def check_message(message):
                     splitted_message = re.split(f'{code_msg}|ст.|ст|статья| ', message)
                     article = get_article(splitted_message)
                     articles = splitCode(code)
-                    return findArticle(article, articles)
+                    answer = findArticle(article, articles)
+                    if answer is None or len(answer) == 0:
+                        return f"В {empty_results[code]} я не нашел статьи {article}"
+                    return answer
                 else:
                     splitted_message = re.split(f'{code_msg}| ', message)
                     if len(splitted_message) > 3 or not any(w for w in splitted_message if len(w) > 0):
@@ -61,9 +78,15 @@ def check_message(message):
                     if bool(re.search(r'\d', word)) and "-" not in word and "." not in word:
                         num_word = num2words(word, lang="ru")
                         word_entries = findWord(word, articles)
-                        num_word_entries = findWord(num_word, articles)
-                        return word_entries.extend(x for x in num_word_entries if x not in word_entries)
-                    return findWord(word[:-1], articles)
+                        num_word_entries = findWord(num_word[:-1], articles)
+                        word_entries.extend([x for x in num_word_entries if x not in word_entries])
+                        if word_entries is None or len(word_entries) == 0:
+                            return f"В {empty_results[code]} я не нашел {word}"
+                        return word_entries
+                    answer = findWord(word[:-1], articles)
+                    if answer is None or len(answer) == 0:
+                        return f"В {empty_results[code]} я не нашел {word}"
+                    return answer
         else:
             return "Вы не указали кодекс!"
     except Exception as e:
