cc213607354e31b5adce6c0f290cda84faced5c1
Aleksey Halayonok <aleksey.halayonok@draytus.com>
2020-03-11 17:00:15 +0300

    Add message generator

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import telebot
 from message_analyzer import check_message
 from EmailSender import EmailSender
 from DbSaver import DataSaver
+from message_generator import generate_start_message
 
 TOKEN = os.environ.get("TelegramToken")
 DOMAIN = os.environ.get("BotDomain")
@@ -12,12 +13,7 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    hello_message = 'Привет, ' + message.from_user.first_name + "\n"
-    hello_message = hello_message + "Бот можно использовать двумя способами:" + "\n" + "1. Ввести название кодекса (ук, УК, уголовный кодекс, Уголовный Кодекс) и номер статьи. Например, ук ст 139, или ук ст.139, или ук статья 139, или ук ст. 139."
-    hello_message = hello_message + "\n\n" + "2. Ввести название кодекса и слово. Я попробую найти это слово в кодексе и покажу все статьи, в которых оно упоминается. Например, ук кража."
-    hello_message = hello_message + "\n\n" + "Подсказка: если ввести ук 20, то я покажу все статьи в УК, в которых есть число 20 либо слово двадцать."
-    hello_message = hello_message + "\n\n" + "Я работаю со всеми кодексами, принятыми в РБ: УК, УПК, ГК, ГПК, КоАП, ПИКоАП, ХПК, ТК, КоБС, НК, Таможенный кожекс, БК (Банковский кодекс), Бюджетный кодекс, Кодекс о земле, ЖК, УИК, Кодекс об образовании, ЛК, Кодекс о недрах, Воздушный кодекс, Водный кодекс, Кодекс о судоустройстве и статусе судей, ИК, Кодекс внутренного водного транспорта, Кодекс торгового мореплавания"
-    bot.reply_to(message, hello_message)
+    bot.reply_to(message, generate_start_message(message))
     db = DataSaver()
     db.save_request_info(message, "OK", "OK")
 
